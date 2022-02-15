2545fb71831e5ab9c362669eee72f69f813de89e
rodrigosevero <rodrigoseverodev@gmail.com>
2022-02-15 16:41:46 -0400

    Ajustes na home na seção noticias e gaat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -176,13 +176,12 @@
 </section>
 <!-- Feature are end -->
 
-<!--/ subscribe end -->
 
 
-<section id="ts-team" class="ts-team">
+<section id="news" class="news">
     <div class="container">
         <div class="row text-center">
-            <div class="col-lg-12">
+            <div class="col-12">
                 <h2 class="section-title">Comunicação</h2>
                 <h3 class="section-sub-title">Assessoria de imprenssa</h3>
             </div>
@@ -190,149 +189,83 @@
         <!--/ Title row end -->
 
         <div class="row">
-            <div class="col-lg-12">
-                <div id="team-slide" class="team-slide">
-                    <div class="item">
-                        <div class="ts-team-wrapper">
-                            <div class="team-img-wrapper">
-                                <img loading="lazy" class="w-100" src="{{ asset('images/team/team1.jpg') }}" alt="team-img">
-                            </div>
-                            <div class="ts-team-content">
-                                <a href="#">
-                                    <h3 class="ts-name">Nats Stenman</h3>
-                                    <!-- <p class="ts-designation">Chief Operating Officer</p> -->
-                                    <p class="ts-description">Nats Stenman began his career in construction with boots on the ground</p>
-                                    <!-- <div class="team-social-icons">
-                                    <a target="_blank" href="#"><i class="fab fa-facebook-f"></i></a>
-                                    <a target="_blank" href="#"><i class="fab fa-twitter"></i></a>
-                                    <a target="_blank" href="#"><i class="fab fa-google-plus"></i></a>
-                                    <a target="_blank" href="#"><i class="fab fa-linkedin"></i></a>
-                                </div> -->
-                                </a>
-
-                                <!--/ social-icons-->
-                            </div>
-                        </div>
-                        <!--/ Team wrapper end -->
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="latest-post">
+                    <div class="latest-post-media">
+                        <a href="#" class="latest-post-img">
+                            <img loading="lazy" class="img-fluid" src="{{ asset('images/news/1.jpg')}}" alt="img">
+                        </a>
                     </div>
-                    <!-- Team 1 end -->
-
-                    <div class="item">
-                        <div class="ts-team-wrapper">
-                            <div class="team-img-wrapper">
-                                <img loading="lazy" class="w-100" src="images/team/team2.jpg" alt="team-img">
-                            </div>
-                            <div class="ts-team-content">
-                                <h3 class="ts-name">Angela Lyouer</h3>
-                                <!-- <p class="ts-designation">Innovation Officer</p> -->
-                                <p class="ts-description">Nats Stenman began his career in construction with boots on the ground</p>
-                                <!-- <div class="team-social-icons">
-                                    <a target="_blank" href="#"><i class="fab fa-facebook-f"></i></a>
-                                    <a target="_blank" href="#"><i class="fab fa-twitter"></i></a>
-                                    <a target="_blank" href="#"><i class="fab fa-linkedin"></i></a>
-                                </div> -->
-                                <!--/ social-icons-->
-                            </div>
+                    <div class="post-body">
+                        <h4 class="post-title">
+                            <a href="#" class="d-inline-block">Sábado tem Dia "D" contra a Covid-19 para crianças em Várzea Grande</a></h4>
+                        </a>
+                        </h4>
+                        <div class="latest-post-meta">
+                            <span class="post-item-date">
+                                <i class="fa fa-clock-o"></i> {{ date("d/m/Y") }}
+                            </span>
                         </div>
-                        <!--/ Team wrapper end -->
                     </div>
-                    <!-- Team 2 end -->
-
-                    <div class="item">
-                        <div class="ts-team-wrapper">
-                            <div class="team-img-wrapper">
-                                <img loading="lazy" class="w-100" src="images/team/team3.jpg" alt="team-img">
-                            </div>
-                            <div class="ts-team-content">
-                                <h3 class="ts-name">Mark Conter</h3>
-                                <!-- <p class="ts-designation">Safety Officer</p> -->
-                                <p class="ts-description">Nats Stenman began his career in construction with boots on the ground</p>
-                                <!-- <div class="team-social-icons">
-                                    <a target="_blank" href="#"><i class="fab fa-twitter"></i></a>
-                                    <a target="_blank" href="#"><i class="fab fa-google-plus"></i></a>
-                                    <a target="_blank" href="#"><i class="fab fa-linkedin"></i></a>
-                                </div> -->
-                                <!--/ social-icons-->
-                            </div>
-                        </div>
-                        <!--/ Team wrapper end -->
-                    </div>
-                    <!-- Team 3 end -->
-
-                    <div class="item">
-                        <div class="ts-team-wrapper">
-                            <div class="team-img-wrapper">
-                                <img loading="lazy" class="w-100" src="images/team/team4.jpg" alt="team-img">
-                            </div>
-                            <div class="ts-team-content">
-                                <h3 class="ts-name">Ayesha Stewart</h3>
-                                <!-- <p class="ts-designation">Finance Officer</p> -->
-                                <p class="ts-description">Nats Stenman began his career in construction with boots on the ground</p>
-                                <!-- <div class="team-social-icons">
-                                    <a target="_blank" href="#"><i class="fab fa-facebook-f"></i></a>
-                                    <a target="_blank" href="#"><i class="fab fa-twitter"></i></a>
-                                    <a target="_blank" href="#"><i class="fab fa-google-plus"></i></a>
-                                    <a target="_blank" href="#"><i class="fab fa-linkedin"></i></a>
-                                </div> -->
-                                <!--/ social-icons-->
-                            </div>
-                        </div>
-                        <!--/ Team wrapper end -->
-                    </div>
-                    <!-- Team 4 end -->
-
-                    <div class="item">
-                        <div class="ts-team-wrapper">
-                            <div class="team-img-wrapper">
-                                <img loading="lazy" class="w-100" src="images/team/team5.jpg" alt="team-img">
-                            </div>
-                            <div class="ts-team-content">
-                                <h3 class="ts-name">Dave Clarkte</h3>
-                                <!-- <p class="ts-designation">Civil Engineer</p> -->
-                                <p class="ts-description">Nats Stenman began his career in construction with boots on the ground</p>
-                                <!-- <div class="team-social-icons">
-                                    <a target="_blank" href="#"><i class="fab fa-twitter"></i></a>
-                                    <a target="_blank" href="#"><i class="fab fa-google-plus"></i></a>
-                                    <a target="_blank" href="#"><i class="fab fa-linkedin"></i></a>
-                                </div> -->
-                                <!--/ social-icons-->
-                            </div>
-                        </div>
-                        <!--/ Team wrapper end -->
-                    </div>
-                    <!-- Team 5 end -->
-
-                    <div class="item">
-                        <div class="ts-team-wrapper">
-                            <div class="team-img-wrapper">
-                                <img loading="lazy" class="w-100" src="images/team/team6.jpg" alt="team-img">
-                            </div>
-                            <div class="ts-team-content">
-                                <h3 class="ts-name">Elton Joe</h3>
-                                <!-- <p class="ts-designation">Site Supervisor</p> -->
-                                <p class="ts-description">Nats Stenman began his career in construction with boots on the ground</p>
-                                <!-- <div class="team-social-icons">
-                                    <a target="_blank" href="#"><i class="fab fa-facebook-f"></i></a>
-                                    <a target="_blank" href="#"><i class="fab fa-twitter"></i></a>
-                                    <a target="_blank" href="#"><i class="fab fa-google-plus"></i></a>
-                                    <a target="_blank" href="#"><i class="fab fa-linkedin"></i></a>
-                                </div> -->
-                                <!--/ social-icons-->
-                            </div>
-                        </div>
-                        <!--/ Team wrapper end -->
-                    </div>
-                    <!-- Team 6 end -->
-
                 </div>
-                <!-- Team slide end -->
+                <!-- Latest post end -->
             </div>
+            <!-- 1st post col end -->
+
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="latest-post">
+                    <div class="latest-post-media">
+                        <a href="#" class="latest-post-img">
+                            <img loading="lazy" class="img-fluid" src="{{ asset('images/news/4.jpg')}}" alt="img">
+                        </a>
+                    </div>
+                    <div class="post-body">
+                        <h4 class="post-title">
+                            <a href="#" class="d-inline-block">Cursos de curta e longa duração estão sendo realizados em Várzea Grande</a>
+                        </h4>
+                        <div class="latest-post-meta">
+                            <span class="post-item-date">
+                                <i class="fa fa-clock-o"></i> {{ date("d/m/Y") }}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <!-- Latest post end -->
+            </div>
+            <!-- 2nd post col end -->
+
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="latest-post">
+                    <div class="latest-post-media">
+                        <a href="#" class="latest-post-img">
+                            <img loading="lazy" class="img-fluid" src="{{ asset('images/news/3.jpg')}}" alt="img">
+                        </a>
+                    </div>
+                    <div class="post-body">
+                        <h4 class="post-title">
+                            <a href="#" class="d-inline-block">Educação promove Formação para Articuladores e Monitores do Programa ETA  </a>
+                        </h4>
+                        <div class="latest-post-meta">
+                            <span class="post-item-date">
+                                <i class="fa fa-clock-o"></i> {{ date("d/m/Y") }}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <!-- Latest post end -->
+            </div>
+            <!-- 3rd post col end -->
         </div>
         <!--/ Content row end -->
+
+        <div class="general-btn text-center mt-4">
+            <a class="btn btn-primary" href="#">Ver todas as notícias</a>
+        </div>
+
     </div>
     <!--/ Container end -->
 </section>
-<!--/ Team end -->
+
 
 
 <section id="news" class="news">
